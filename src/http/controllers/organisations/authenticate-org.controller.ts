9664472491430e7b39bcf19c41d4e799003aad32
Isadora Pilau <isadorapda@gmail.com>
2023-06-17 22:28:54 +0100

    style: 💄 eslint, formatting

--- src/http/controllers/organisations/authenticate-org.controller.ts
+++ src/http/controllers/organisations/authenticate-org.controller.ts
@@ -2,28 +2,28 @@ import { PrismaOrganisationsRepository } from '@/repositories/prisma/prisma-orga
 import { AuthenticateOrganisationService } from '@/services/authenticate-org.service'
 import { InvalidCredentialsError } from '@/services/errors/invalid-credentials-error'
 import { FastifyReply, FastifyRequest } from 'fastify'
-import {z} from 'zod'
+import { z } from 'zod'
 
-export async function authenticateOrganisationController(request:FastifyRequest, reply: FastifyReply){
+export async function authenticateOrganisationController(request: FastifyRequest, reply: FastifyReply) {
 	const authenticateOrganisationBodySchema = z.object({
 		email: z.string().email(),
 		password: z.string().min(6),
 	})
 
-	const {email,password} = authenticateOrganisationBodySchema.parse(request.body)
+	const { email, password } = authenticateOrganisationBodySchema.parse(request.body)
 
 	try {
 		const organisationRepository = new PrismaOrganisationsRepository()
 		const authService = new AuthenticateOrganisationService(organisationRepository)
 
-		const {organisation}=await authService.authenticateOrgService({
+		const { organisation } = await authService.authenticateOrgService({
 			email,
 			password,
 		})
 		const token = await reply.jwtSign(
 			{},
 			{
-				sign:{
+				sign: {
 					sub: organisation.id,
 				}
 			}
@@ -32,18 +32,18 @@ export async function authenticateOrganisationController(request:FastifyRequest,
 		const refreshToken = await reply.jwtSign(
 			{},
 			{
-				sign:{
-					sub:organisation.id,
+				sign: {
+					sub: organisation.id,
 					expiresIn: '7d',
 				}
 			}
 		)
-		return reply.setCookie('refreshToken', refreshToken,{
+		return reply.setCookie('refreshToken', refreshToken, {
 			path: '/',
 			httpOnly: true,
 			secure: true,
-			sameSite:true
-		}).status(200).send({token})
+			sameSite: true
+		}).status(200).send({ token })
 	} catch (error) {
 
 		throw new InvalidCredentialsError()
--- src/utils/create-and-authenticate-org.ts
+++ src/utils/create-and-authenticate-org.ts
@@ -1,13 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateOrganisation(app:FastifyInstance){
+export async function createAndAuthenticateOrganisation(app: FastifyInstance) {
 	await request(app.server).post('/organisations').send({
 		name: 'Pet Org',
 		city: 'London',
 		postcode: 'AB12 3CD',
 		email: 'pet.adopt@email.com',
-		mobile:'447123456789',
+		mobile: '447123456789',
 		address: '',
 		password: '123456',
 	})
@@ -16,7 +16,7 @@ export async function createAndAuthenticateOrganisation(app:FastifyInstance){
 		email: 'pet.adopt@email.com',
 		password: '123456',
 	})
-	const {token} = authResponse.body
+	const { token } = authResponse.body
 
-	return {token}
+	return { token }
 }
