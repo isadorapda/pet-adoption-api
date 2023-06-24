53a8b2c348beb544ecef6cb4d36bf4c1c959ead6
Isadora Pilau <isadorapda@gmail.com>
2023-06-24 11:50:41 +0100

    feat: 🎸 util function to remove password

---
+++ src/utils/removePasswordHash.ts
@@ -0,0 +1,11 @@
+import { Organisation } from '@prisma/client'
+
+export function removePasswordHash(input: Organisation) {
+	const { password_hash: _password_hash, ...org } = input
+
+	return org
+}
+
+export function removePasswordHashes(input: Array<Organisation>) {
+	return input.map(removePasswordHash)
+}
